54db49a3fae62c8846cdc883824e034178af703a
Salman <salman.sadiq@cowlar.com>
2024-07-22 17:53:47 +0500

    Added  Main libraries

--- main/main.c
+++ main/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "mqtt_client.h"
 void app_main(void)
 {
 
